b69bfde33b66c9ca493bbe27eedf0c2250e342ed
Shanks <84927221+SHANKS-00@users.noreply.github.com>
2021-09-05 18:07:14 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class Add {
+    public static void main(String args[]) {
+      int e=10;
+      int f=25;
+      int g=e+f;
+
+      System.out.println("The sum of e+f is = " + g);
+    }
+}
